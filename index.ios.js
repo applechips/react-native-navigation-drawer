c0a8ceda02504f1524ed4c4a86eee7102b1a4a98
Nicole <nhswong@gmail.com>
2017-02-10 19:40:07 -0800

    add drawer component

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,43 +5,40 @@ import {
   Text,
   View
 } from 'react-native';
+import Drawer from 'react-native-drawer';
 
 export default class NavigationDrawer extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Drawer>
+        ref={ (ref) => this._drawer = ref }
+        type="overlay"
+        content={<Menu />}
+        tapToClose={true}
+        openDrawerOffset={0.2}
+        panCloseMask={0.2}
+        closedDrawerOffset={-3}
+        styles={{
+          drawer: {shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3},
+          main: {paddingLeft: 3}
+        }}
+        tweenHandler={(ratio) => ({
+          main: { opacity:(2-ratio)/2 }
+        })}>
+
+        {/* Navigator component will be in here, for now we will just add a view: */}
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+              Welcome to React Native!
+          </Text>
+        </View>
+      </Drawer>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('NavigationDrawer', () => NavigationDrawer);
